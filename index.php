80743d72e91cc7c4e9ff114bfbf449cbfafd31d9
Moreaulouis <moreau.louismichel@gmail.com>
2019-12-04 10:00:15 +0100

    C'est moi qu'a trouvé pour celui ci

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <?php function chaine ($para = 'Yo', $param = 'Billy')
+  {
+      return ($para . ' ' . $param);
+  }
+echo chaine ()
+  ?>
+
+</body>
+</html>
